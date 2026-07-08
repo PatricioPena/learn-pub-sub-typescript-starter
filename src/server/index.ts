ac163958c4a044bdcb2bc95d47c3083041669294
Patricio <patricio17po@gmail.com>
2026-07-08 08:22:48 -0600

    CH2-L3

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,17 @@
+import amqp from "amqplib";
+import process from 'node:process';
+
 async function main() {
   console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection was successful");
+
+
+  process.on('SIGINT', async () => {
+    console.log(`Signal received, shutting down`);
+    await conn.close();
+  });
 }
 
 main().catch((err) => {
