d4a334eda625784a2cde82aa0f64b491be2c2fb0
Patricio <patricio17po@gmail.com>
2026-07-23 11:40:32 -0600

    CH6-L1

--- src/internal/pubsub/publish.ts
+++ src/internal/pubsub/publish.ts
@@ -1,4 +1,5 @@
 import type { ConfirmChannel } from "amqplib";
+import { encode } from "@msgpack/msgpack";
 
 export async function publishJSON<T>(
     ch: ConfirmChannel,
@@ -8,6 +9,28 @@ export async function publishJSON<T>(
 ): Promise<void> {
     const strVal = JSON.stringify(value);
     const newVal = Buffer.from(strVal);
-    ch.publish(exchange, routingKey, newVal, {contentType: "application/json"} );
+    ch.publish(exchange, routingKey, newVal, { contentType: "application/json" });
     return;
+}
+
+export function publishMsgPack<T>(
+    ch: ConfirmChannel,
+    exchange: string,
+    routingKey: string,
+    value: T,
+): Promise<void> {
+    const strVal = encode(value);
+    const newVal = Buffer.from(strVal);
+    return new Promise((resolve, reject) => {
+        ch.publish(exchange, routingKey, newVal, { contentType: "application/x-msgpack" },
+            (err) => {
+                if (err !== null) {
+                    reject(new Error("Message was NACKed by the broker"));
+                }
+                else {
+                    resolve()
+                }
+            }
+        );
+    });
 }
